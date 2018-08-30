5b9acd08f91c6cc8edb9fe14d6e9fd49d9d897bb
Youngjin-KimY <11kimyj@gmail.com>
2018-08-31 01:04:53 +0900

    init new repo

---
+++ app.js
@@ -0,0 +1,49 @@
+new Vue({
+    el:"#vue-app",
+    data:{
+        age:20,
+        a:0,
+        b:0
+        // // x:0,
+        // // y:0
+        // name:'',
+        // age:''
+    },
+    // methods:{
+    //     greet:function (time) {
+    //         return 'Good'+time+''+this.name;
+    //     },
+    //     add:function (inc) {
+    //         this.age+=inc;
+    //     },
+    //     subtract:function (dec) {
+    //         this.age-=dec;
+    //     },
+    //     updateXY: function (event) {
+    //         this.x = event.offsetX;
+    //         this.y = event.offsetY;
+    //     },
+    //     logName: function () {
+    //
+    //     },
+    //     logAge: function () {
+    //
+    //     },
+    //     addToA:function(){
+    //         return this.a+this.age;
+    //     },
+    //     addToB:function () {
+    //         return this.b+this.age;
+    //
+    //     }
+    // },
+    computed:{
+        addToA:function(){
+            return this.a+this.age;
+        },
+        addToB:function () {
+            return this.b+this.age;
+
+        }
+    }
+});
